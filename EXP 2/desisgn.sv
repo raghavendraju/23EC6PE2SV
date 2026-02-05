51f400ed66ff51b9732e4eb67a5c166f938d88e8
Raghavendra Raju <rajuraghavendra65@gmail.com>
2026-02-05 12:35:39 +0530

    EXP2

---
+++ EXP 2/desisgn.sv
@@ -0,0 +1,18 @@
+
+//------------------------------------------------------------------------------
+//File       : design.sv
+//Author     : Raghavendra (1BM23EC200)
+//Created    : 2026-02-01
+//Module     : mux2to1
+//Project    : SystemVerilog and Verification (23EC6PE2SV),
+//Faculty    : Prof. Ajaykumar Devarapalli
+//Description: 2-input Multipexer used for basic functional coverage example.
+//------------------------------------------------------------------------------
+
+module mux2to1 (
+  input logic [7:0] a, b,
+  input logic sel,
+  output logic [7:0] y
+);
+  assign y = sel ? b : a;
+endmodule
