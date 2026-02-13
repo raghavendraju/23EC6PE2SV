5182c621d23f3ba115ac5ad42e32a9fda5cdda3e
Raghavendra Raju <rajuraghavendra65@gmail.com>
2026-02-13 17:04:22 +0530

    digital clock

---
+++ TASK 1 (DIGITAL CLOCK)/design.sv
@@ -0,0 +1,37 @@
+// -----------------------------------------------------------------------------
+// File        : digital_clock.sv
+// Author      : Raghavendra Raju (1BM23EC200)
+// Created     : 2026-02-10
+// Module      : counter
+// Project     : SystemVerilog and Verification (23EC6PE2SV),
+//               Faculty: Prof. Ajaykumar Devarapalli
+// Description : Digital clock that tracks time using counters.
+// ----------------------------------------------------------------------------- 
+module digital_clock (
+    input  logic clk,
+    input  logic rst,
+    output logic [5:0] sec,
+    output logic [5:0] min
+);
+
+    always_ff @(posedge clk or posedge rst) begin
+        if (rst) begin
+            sec <= 0;
+            min <= 0;
+        end
+        else begin
+            if (sec == 59) begin
+                sec <= 0;
+
+                if (min == 59)
+                    min <= 0;
+                else
+                    min <= min + 1;
+            end
+            else begin
+                sec <= sec + 1;
+            end
+        end
+    end
+
+endmodule
